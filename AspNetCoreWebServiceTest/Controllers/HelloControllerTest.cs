43bb1496e9b9f8d7f0b452c5de7a7e7123ebe13b
Kenneth Phang <bunnyppl@gmail.com>
2019-09-12 11:45:10 +0800

    fail test case

--- AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
+++ AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
@@ -10,7 +10,7 @@ namespace AspNetCoreWebServiceTest.Controllers
         {
             HelloController controller = new HelloController();
             var response = controller.Get().Value as Response;
-            Assert.Equal("Hello World!", response.output);
+            Assert.Equal("Hello World!!", response.output);
         }
 
         [Theory]
